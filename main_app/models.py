5da8957b506cb223c04cb847ea4e87cf7d072fd5
SelamB <selambeyene2@gmail.com>
2023-12-09 21:55:19 -0800

    adds Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
@@ -10,3 +16,18 @@ class Finch(models.Model):
         return self.name
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+    # Create foreign key for the finch
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
