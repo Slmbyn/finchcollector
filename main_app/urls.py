20227bafb6796d6e5dbf992b7b5fc862cc96c68d
SelamB <selambeyene2@gmail.com>
2023-12-09 19:55:11 -0800

    adds create functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('allfinches/', views.allfinches, name='allfinches'),
-    path('allfinches/<int:finch_id>/', views.finch_detail, name='detail')
+    path('allfinches/<int:finch_id>/', views.finch_detail, name='detail'),
+    path('allfinches/create', views.FinchCreate.as_view(), name='finches_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def home (request):
@@ -18,5 +19,9 @@ def finch_detail (request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', {'finch':finch})
 
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    # success_url = '/allfinches/{finch.id}'
 # create fake finch data ( a list with dictionaries inside)
 # then pass that data to allfinches page by including it as the 3rd parameter in the render
