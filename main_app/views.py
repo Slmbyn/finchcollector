d74308a01299f17b29517f5162b8b112f34529e3
SelamB <selambeyene2@gmail.com>
2023-12-09 18:46:12 -0800

    creates Finch model

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
 def home (request):
@@ -8,5 +9,10 @@ def about (request):
     return render(request, 'about.html')
 
 def allfinches (request):
-    return render(request, 'finches/index.html')
+    finch = Finch.objects.all()
+    return render(request, 'finches/index.html', {
+        'finches': finch
+    })
 
+# create fake finch data ( a list with dictionaries inside)
+# then pass that data to allfinches page by including it as the 3rd parameter in the render
